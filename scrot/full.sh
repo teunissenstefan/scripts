7018cf743829c4fd1f2a7a4fae379cf4ab2d79ae
Stefan Teunissen <stefanteunissen1@gmail.com>
2020-03-01 18:15:49 +0100

    Scrot scripts

---
+++ scrot/full.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scrot -b '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/$f && exec notify-send "Screenshot taken"'

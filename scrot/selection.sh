70607e4e38d3e3aea1d66af79eb3a4fbed8ece22
Stefan Teunissen <stefanteunissen1@gmail.com>
2020-06-09 23:05:02 +0200

    Scrot selection improved with slop

--- scrot/selection.sh
+++ scrot/selection.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-scrot -s '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/$f && exec notify-send "Screenshot taken"'
+#scrot -s '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/$f && exec notify-send "Screenshot taken"'
+VECTORS=$(slop -f '%x,%y,%w,%h' -q)
+
+if [ -z "${VECTORS}" ]; then
+    notify-send "Selection cancelled"
+else
+    scrot -a "${VECTORS}" '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/$f && exec notify-send "Screenshot taken"'
+fi

66af5fffbe7eacd2f5896da867eb367de2b64ffd
Stefan Teunissen <stefan@bigbridge.nl>
2021-12-14 15:22:58 +0100

    slop selection crosshair

--- scrot/selection.sh
+++ scrot/selection.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #scrot -s '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/$f && exec notify-send "Screenshot taken"'
-VECTORS=$(slop -f '%x,%y,%w,%h' -q)
+VECTORS=$(slop -r crosshair -f '%x,%y,%w,%h' -q)
 
 if [ -z "${VECTORS}" ]; then
     notify-send "Selection cancelled"
